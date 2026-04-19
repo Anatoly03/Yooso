25bc7fd039018f289654ff948abd2b49655deca5
Anatoly Wein <anatoly.wein@gmail.com>
2026-04-19 14:12:44 +0200

    add collection row save method

--- yooso-macro/src/macro_collection.rs
+++ yooso-macro/src/macro_collection.rs
@@ -21,19 +21,35 @@ pub fn collection(table_metadata: CollectionMeta, mut strucc: ItemStruct) -> Tok
     let table_name = &table_metadata.table;
     let db_struct_name = &table_metadata.db;
 
+    // Generate [FieldMeta] for each field and consume any field-level
+    // attributes like #[primary].
+    let field_metas = strucc
+        .fields
+        .iter()
+        .map(|field| FieldMeta::from(field.clone()))
+        .collect::<Vec<_>>();
+
+    // Consume "#[primary]" attributes on fields.
+    for field in &mut strucc.fields {
+        field
+            .attrs
+            .retain(|attr| !attr.meta.path().is_ident("primary"));
+    }
+
+    // Validate fields are 'named', not 'unnamed' or 'unit'.
+    let fields = match &strucc.fields {
+        Fields::Named(fields_named) => &fields_named.named,
+        Fields::Unnamed(_) => panic!("#[collection] struct must have named fields"),
+        Fields::Unit => panic!("#[collection] cannot be a unit struct"),
+    };
+
     // Generate 'CREATE TABLE' syntax for the table generator.
     let sql_create_table = {
         // This generates a vector of field definitions.
-        let sql_create_table_fields = match &strucc.fields {
-            Fields::Named(fields_named) => fields_named
-                .named
-                .iter()
-                .map(|field| Into::<FieldMeta>::into(field.clone()))
-                .map(|field_meta| field_meta.into_field_definition())
-                .collect::<Vec<_>>(),
-            Fields::Unnamed(_) => panic!("#[collection] struct must have named fields"),
-            Fields::Unit => panic!("#[collection] cannot be a unit struct"),
-        };
+        let sql_create_table_fields = field_metas
+            .iter()
+            .map(|field_meta| field_meta.into_field_definition())
+            .collect::<Vec<_>>();
 
         format!(
             "CREATE TABLE IF NOT EXISTS {} ({})",
@@ -42,10 +58,68 @@ pub fn collection(table_metadata: CollectionMeta, mut strucc: ItemStruct) -> Tok
         )
     };
 
-    // Consume "#[primary]" attributes on fields.
-    for field in &mut strucc.fields {
-        field.attrs.retain(|attr| !attr.meta.path().is_ident("primary"));
-    }
+    // Generate 'INSERT INTO' syntax for the insert generator.
+    let sql_insert_into = {
+        let field_names = fields
+            .iter()
+            .map(|field| field.ident.as_ref().unwrap().to_string())
+            .collect::<Vec<_>>();
+
+        let indeces = (0..field_names.len())
+            .map(|i| format!("${}", i + 1))
+            .collect::<Vec<_>>();
+
+        format!(
+            "INSERT OR REPLACE INTO {} ({}) VALUES ({})",
+            table_name.value(),
+            field_names.join(", "),
+            indeces.join(", ")
+        )
+    };
+
+    // Generate 'DELETE FROM ... WHERE' syntax for the delete generator.
+    let sql_delete_from = {
+        let primary_keys = field_metas
+            .iter()
+            .filter(|field_meta| field_meta.primary)
+            .map(|field_meta| field_meta.name.clone())
+            .collect::<Vec<_>>();
+
+        if primary_keys.is_empty() {
+            panic!("#[collection] must have at least one #[primary] field");
+        }
+
+        let where_clause = primary_keys
+            .iter()
+            .enumerate()
+            .map(|(i, key)| format!("{} = ${}", key, i + 1))
+            .collect::<Vec<_>>()
+            .join(" AND ");
+
+        format!(
+            "DELETE FROM {} WHERE {}",
+            table_name.value(),
+            where_clause
+        )
+    };
+
+    // Generate a list of field identifiers. This is used for the insert generator
+    // to extract field values.
+    let field_ids = fields
+        .iter()
+        .map(|field| {
+            // The field type 'Uuid' is a special case because Uuid does
+            // not implement 'rusqlite::ToSql' directly.
+            let ident = field.ident.as_ref().unwrap();
+
+            // For Uuid fields, we will call 'to_string()' on the field value
+            // to convert it to a string before inserting into the database.
+            match &field.ty {
+                syn::Type::Path(type_path) if type_path.path.is_ident("Uuid") => quote! { #ident.to_string() },
+                _ => quote! { #ident }
+            }
+        })
+        .collect::<Vec<_>>();
 
     quote! {
         #strucc
@@ -62,17 +136,26 @@ pub fn collection(table_metadata: CollectionMeta, mut strucc: ItemStruct) -> Tok
             /// ```
             pub const TABLE_NAME: &'static str = #table_name;
 
-            /// Invoke table generator query for this collection. This will execute
-            /// the following SQL query
+            /// Invoke table generator query for this collection.
+            /// 
+            /// # Query
+            /// 
+            /// This method will execute the following SQL query.
             ///
             /// ```sql
             #[doc = #sql_create_table]
             /// ```
+            /// 
+            /// # Returns
+            /// 
+            /// This method returns the number of rows affected by the SQLite query.
+            /// For 'CREATE TABLE' queries, this always yields `0`.
             pub async fn create_table() -> usize {
                 let db = #db_struct_name::connect();
 
-                // Execute the CREATE SQL statement to create the collection table if it doesn't exist.
-                // Returns the number of rows affected (should be 0 for CREATE TABLE).
+                // Execute the CREATE SQL statement to create the collection table
+                // if it doesn't exist. Returns the number of rows affected (should
+                // be 0 for CREATE TABLE).
                 db.lock()
                     .expect("lock db mutex")
                     .execute(#sql_create_table, [])
@@ -80,6 +163,59 @@ pub fn collection(table_metadata: CollectionMeta, mut strucc: ItemStruct) -> Tok
 
                 // #[cfg(feature = "debug")]
                 // assert_eq!(..., 0, "CREATE TABLE should not affect any rows");
+            }
+
+            /// Saves the current struct instance as a new row in the collection's
+            /// table.
+            /// 
+            /// # Query
+            /// 
+            /// This method will execute the following SQL query.
+            ///
+            /// ```sql
+            #[doc = #sql_insert_into]
+            /// ```
+            /// 
+            /// # Returns
+            /// 
+            /// This method returns the number of rows affected by the SQLite query.
+            /// If the row was inserted for the first time, this yields `1`. If a row
+            /// with the same primary key already exists, this yields `0`, overriding
+            /// the existing row.
+            pub async fn save(&self) -> usize {
+                let db = #db_struct_name::connect();
+
+                // Execute the INSERT SQL statement to insert a new row into the
+                // collection table. Returns the number of rows affected (should
+                // be 1 for INSERT and 0 for REPLACE).
+                // If row with same key exists, it will be overridden.
+                db.lock()
+                    .expect("lock db mutex")
+                    .execute(#sql_insert_into, ::rusqlite::params![
+                        #(&self.#field_ids),*
+                    ])
+                    .expect("insert into collection table")
+            }
+
+            /// Deletes the row corresponding to the current struct instance from the
+            /// collection's table. The row is identified by the primary key fields
+            /// of the struct.
+            /// 
+            /// # Query
+            /// 
+            /// This method will execute the following SQL query.
+            /// 
+            /// ```sql
+            #[doc = #sql_delete_from]
+            /// ```
+            /// 
+            /// # Returns
+            /// 
+            /// This method returns the number of rows affected by the SQLite query.
+            /// If a row was deleted, this yields `1`. If no matching row was found
+            /// to delete, this yields `0`.
+            pub async fn delete(&self) -> usize {
+                todo!("delete method not implemented yet")
             }
         }
     }
