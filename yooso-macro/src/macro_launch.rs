a6fa6bd049631ecde4e0a53b1c1c50a23144b4c8
Anatoly Wein <anatoly.wein@gmail.com>
2026-05-01 12:59:44 +0200

    refactor launch macro documentation

--- yooso-macro/src/macro_launch.rs
+++ yooso-macro/src/macro_launch.rs
@@ -6,6 +6,8 @@ use syn::ItemFn;
 /// application and turns it into the program entry point.
 pub fn launch(mut function: ItemFn) -> TokenStream {
     let launch_fn = format_ident!("__yooso_launch_{}", function.sig.ident);
+    let original_fn_vis = function.vis.clone();
+    let original_fn_attr = function.attrs.clone();
 
     function.sig.ident = launch_fn.clone();
 
@@ -13,7 +15,26 @@ pub fn launch(mut function: ItemFn) -> TokenStream {
         #[doc(hidden)]
         #function
 
-        fn main() {
+        #(#original_fn_attr)*
+        ///
+        /// # Macro Expansion
+        /// 
+        /// The function marked with [yooso::launch] will be renamed internally
+        /// to `__yooso_launch_yooso` and wrapped into an async [main] function.
+        /// 
+        /// ```no_run
+        #[doc = concat!(stringify!(#original_fn_vis fn main()), " {")]
+        ///     ::yooso::rocket::tokio::runtime::Builder
+        ///         ::new_multi_thread()
+        ///         .enable_all()
+        ///         .build()
+        ///         .expect("failed to build Tokio runtime")
+        ///         .block_on(async {
+        #[doc = concat!("\t\t\t\t", stringify!(#launch_fn().await.launch().await;))]
+        ///         });
+        /// }
+        /// ```
+        #original_fn_vis fn main() {
             ::yooso::rocket::tokio::runtime::Builder::new_multi_thread()
                 .enable_all()
                 .build()
