c8512684a47fbc0fea8d7530e95a643c266b672f
Anatoly03 <Anatoly03@users.noreply.github.com>
2026-06-26 14:58:59 +0000

    deploy: 2eb6f615a9298972c6536e233f9f90e41539bd98

--- trait.impl/util_validation/trait.ValidateFrom.js
+++ trait.impl/util_validation/trait.ValidateFrom.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["util_validation",[]],["yooso_storage",[["impl <a class=\"trait\" href=\"util_validation/trait.ValidateFrom.html\" title=\"trait util_validation::ValidateFrom\">ValidateFrom</a>&lt;<a class=\"struct\" href=\"yooso_storage/struct.ComponentFieldRecord.html\" title=\"struct yooso_storage::ComponentFieldRecord\">ComponentFieldRecord</a>&gt; for <a class=\"struct\" href=\"yooso_storage/struct.ComponentFieldRecord.html\" title=\"struct yooso_storage::ComponentFieldRecord\">ComponentFieldRecord</a>",0],["impl <a class=\"trait\" href=\"util_validation/trait.ValidateFrom.html\" title=\"trait util_validation::ValidateFrom\">ValidateFrom</a>&lt;<a class=\"struct\" href=\"yooso_storage/struct.ComponentRecord.html\" title=\"struct yooso_storage::ComponentRecord\">ComponentRecord</a>&gt; for <a class=\"struct\" href=\"yooso_storage/struct.ComponentRecord.html\" title=\"struct yooso_storage::ComponentRecord\">ComponentRecord</a>",0]]]]);
+    const implementors = Object.fromEntries([["util_validation",[]],["yooso_storage",[["impl ValidateFrom&lt;<a class=\"struct\" href=\"yooso_storage/struct.ComponentFieldRecord.html\" title=\"struct yooso_storage::ComponentFieldRecord\">ComponentFieldRecord</a>&gt; for <a class=\"struct\" href=\"yooso_storage/struct.ComponentFieldRecord.html\" title=\"struct yooso_storage::ComponentFieldRecord\">ComponentFieldRecord</a>",0],["impl ValidateFrom&lt;<a class=\"struct\" href=\"yooso_storage/struct.ComponentRecord.html\" title=\"struct yooso_storage::ComponentRecord\">ComponentRecord</a>&gt; for <a class=\"struct\" href=\"yooso_storage/struct.ComponentRecord.html\" title=\"struct yooso_storage::ComponentRecord\">ComponentRecord</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[22,912]}
+//{"start":59,"fragment_lengths":[22,676]}
